1aa174745a5d6ab00625765ccb4eb2608efaa9a2
David <david.duregu@gmail.com>
2023-02-14 23:07:43 -0500

    Código java añadido, encriptado y desencriptado agregado

---
+++ Codigo.js
@@ -0,0 +1,35 @@
+const inputMensaje = document.querySelector("#mensaje");
+const inputResultado = document.querySelector("#resultado");
+
+const btnEncriptar = document.querySelector("#encriptar");
+const btnDesencriptar = document.querySelector("#desencriptar");
+const btnCopiar = document.querySelector("#copiar");
+const btnEscuchar = document.querySelector("#escuchar");
+
+function encriptar (){
+    var mensaje = inputMensaje.value;
+    var mensajeEncriptado = mensaje
+    .replaceAll("e","enter")
+    .replaceAll("i","imes")
+    .replaceAll("o","ober")
+    .replaceAll("a","ai")
+    .replaceAll("u","ufat");
+
+    inputResultado.value = mensajeEncriptado;
+}
+
+function desencriptar (){
+    var mensajeEncriptado = inputMensaje.value;
+    var mensaje = mensajeEncriptado
+    .replaceAll("enter","e")
+    .replaceAll("imes","i")
+    .replaceAll("ober","o")
+    .replaceAll("ai","a")
+    .replaceAll("ufat","u");
+
+    inputResultado.value = mensaje;
+}
+
+
+btnEncriptar.onclick = encriptar;
+btnDesencriptar.onclick = desencriptar;
